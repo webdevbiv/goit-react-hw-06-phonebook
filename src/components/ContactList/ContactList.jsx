61e6a64d922d4e0816a34c8968b028ff741a89b1
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-04-14 23:31:25 +0300

    01

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,31 +1,33 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import s from '../ContactList/ContactList.module.scss'
+import s from '../ContactList/ContactList.module.scss';
 function ContactList({ contacts, onDelete }) {
-    return (
-        <ListGroup>
-            {contacts.map((contact) => (
-                <ListGroup.Item
-                    key={contact.id}
-                    className={s.item}
-                >
-                    <div>{contact.name}: {contact.number}</div>
-                    <Button variant="primary" type="button"
-                        onClick={() => onDelete(contact.id)}
-                    >
-                        Delete</Button>
-                </ListGroup.Item>
-            ))}
-        </ListGroup>
-    )
+  return (
+    <ListGroup>
+      {contacts.map(contact => (
+        <ListGroup.Item key={contact.id} className={s.item}>
+          <div>
+            {contact.name}: {contact.number}
+          </div>
+          <Button
+            variant="primary"
+            type="button"
+            onClick={() => onDelete(contact.id)}
+          >
+            Deletes
+          </Button>
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
 }
 
 ContactList.propTypes = {
-    contacts: PropTypes.array.isRequired,
-    onDelete: PropTypes.func.isRequired
-}
+  contacts: PropTypes.array.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
 
-export default ContactList
+export default ContactList;
