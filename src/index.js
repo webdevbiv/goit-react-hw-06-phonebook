65952d6d71adca43698c5eccfe60efff3f0dbef5
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-04-20 22:31:02 +0300

    01

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.scss';
 import { Provider } from 'react-redux';
 
 import { PersistGate } from 'redux-persist/integration/react';
-import { store } from 'redux/store';
+import { store, persistor } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
