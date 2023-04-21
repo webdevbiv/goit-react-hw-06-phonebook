4ff0977cd3a047c9df3fa079a762c7135cf9d328
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-04-21 21:31:01 +0300

    changes

--- src/redux/clicksSlice.js
+++ src/redux/clicksSlice.js
@@ -1,8 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// import { createSlice } from '@reduxjs/toolkit';
+// import { persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+// const clicksSlice = createSlice({
+//   name: 'clicks',
+//   initialState: { value: 0 },
+//   reducers: {
+//     update(state) {
+//       state.value += 1;
+//     },
+//   },
+// });
+// const persistConfig = {
+//   key: 'root',
+//   storage: storage,
+//   blacklist: [''],
+// };
+// export const persistedClicksReducer = persistReducer(
+//   persistConfig,
+//   clicksSlice.reducer
+// );
+// export const { update } = clicksSlice.actions;
 
-const clicksSlice = createSlice({
+import { createSlice } from '@reduxjs/toolkit';
+
+export const clicksSlice = createSlice({
   name: 'clicks',
   initialState: { value: 0 },
   reducers: {
@@ -11,16 +32,5 @@ const clicksSlice = createSlice({
     },
   },
 });
-
-const persistConfig = {
-  key: 'root',
-  storage: storage,
-  blacklist: [''], // navigation will not be persisted
-};
-
-export const persistedClicksReducer = persistReducer(
-  persistConfig,
-  clicksSlice.reducer
-);
 
 export const { update } = clicksSlice.actions;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,29 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import { presistStore } from 'redux-persist';
+// import { clicksReducer } from './clicksSlice';
+// export const store = configureStore({
+//   reducer: {
+//     contacts: [],
+//     filter: '',
+//     clicks: clicksReducer,
+//   },
+// });
+// export const presistor = presistStore(store);
+
 import { configureStore } from '@reduxjs/toolkit';
-import { presistStore } from 'redux-persist';
-import { clicksReducer } from './clicksSlice';
+import { clicksSlice } from './clicksSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import { storage } from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedClicksReducer = persistReducer(persistConfig);
 
 export const store = configureStore({
   reducer: {
-    contacts: [],
-    filter: '',
-    clicks: clicksReducer,
+    clicks: clicksSlice.reducer,
   },
 });
-
-export const presistor = presistStore(store);
