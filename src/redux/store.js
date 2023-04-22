20acf9c6fed630ab2b7c50aebe9da883be901178
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-04-22 16:22:28 +0300

    error

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,13 +11,28 @@
 // export const presistor = presistStore(store);
 
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore } from 'redux-persist';
+
 import { clicksReducer } from './clicksSlice';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 export const store = configureStore({
   reducer: {
     clicks: clicksReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
